73e7c2b519e06b76dedf4ff062ea484c5a2df13b
Theking4121 <47257001+Theking4121@users.noreply.github.com>
2026-03-08 20:17:45 +0100

    Update automated_reset.py for L3 switch and L2 switch black

--- automated_reset.py
+++ automated_reset.py
@@ -2,10 +2,14 @@ import serial
 import datetime
 import threading
 import time
+import json
 
-ser = serial.Serial('/dev/cu.usbserial-A9ADVKYL', baudrate=9600, timeout=1, parity=serial.PARITY_NONE, bytesize=8, stopbits=serial.STOPBITS_ONE, xonxoff=False)
+timeout_time = 5
 
-def Read_from_port(ser,rommon_event ,image_event, prompt_event, switch_break_event):
+ser = serial.Serial('/dev/tty.usbserial-A9ADPXH5', baudrate=9600, timeout=1, parity=serial.PARITY_NONE, bytesize=8, stopbits=serial.STOPBITS_ONE, xonxoff=False)
+
+def Read_from_port(ser,rommon_event ,image_event, prompt_event,multilayer_event , switch_break_event, l2l3_old_event, flash_init_event, read_only_prompt_event):
+    log_file = open("serial.log", "a")
     line_buffer = ""
     while ser.is_open == True:
         if ser.in_waiting > 0:
@@ -13,40 +17,58 @@ def Read_from_port(ser,rommon_event ,image_event, prompt_event, switch_break_eve
             line_buffer += chunk
             lines = line_buffer.split("\n")
             for line in lines[:-1]:
+                
+                #Checks if the router has entered rommon
+                if "rommon" in line:rommon_event.set()
 
-                if "rommon" in line:
-                    rommon_event.set()
+                #Checks when break should be sent to router
+                if "Rom image verified correctly" in line:image_event.set()
 
-                if "Rom image verified correctly" in line:
-                    image_event.set()
+                #Checks when break should be sent to Switch
+                if "Send break" in line:switch_break_event.set()
+                
+                #ML Switch? 
+                if "(interrupted)" in line:multilayer_event.set()
+                
+                #Answears the yes no prompt
+                if "Press RETURN to get started!" in line:prompt_event.set()
+                
+                #black l2/l3 switch
+                if "The system has been interrupted" in line: l2l3_old_event.set()
 
-                if "Send break" in line:
-                    switch_break_event.set()
-
-                if "Press RETURN to get started!" in line:
-                    prompt_event.set()
+                #Initiazling flash on black switches l2/l3
+                if "...done Initializing Flash." in line: flash_init_event.set()
+                
+                #Checks if read only
+                if "read only file system" in line: read_only_prompt_event.set()
 
                 timestamp = datetime.datetime.now().strftime("%H:%M:%S")
                 print(f"{timestamp} {line}")
+
             line_buffer = lines[-1]
 
+read_only_prompt_reached = threading.Event()
 switch_break_reached = threading.Event()
 prompt_reached = threading.Event()
 image_reached = threading.Event()
 rommon_reached = threading.Event()
-threading.Thread(target=Read_from_port, args=(ser, rommon_reached, image_reached, prompt_reached, switch_break_reached), daemon=True).start()
+multilayer_reached = threading.Event()
+l2l3_old_reached = threading.Event()
+flash_init_reached = threading.Event()
+threading.Thread(target=Read_from_port, args=(ser, rommon_reached, image_reached, prompt_reached, multilayer_reached, switch_break_reached, l2l3_old_reached, flash_init_reached, read_only_prompt_reached), daemon=True).start()
 
-while not switch_break_reached.is_set() or not image_reached.is_set():
+while True:
     if switch_break_reached.is_set():
-        ser.send_break()
+        ser.write(bytes(str(f"{ser.send_break()}\r"), "ISO-8859-1"))
         break
     elif image_reached.is_set():
-        ser.send_break()
-        if rommon_reached.is_set():
-            break
+        ser.write(bytes(str(f"{ser.send_break()}\r"), "ISO-8859-1"))
+        if rommon_reached.is_set(): break
+    elif multilayer_reached.is_set(): break
+    elif l2l3_old_reached.is_set(): break
 
 if switch_break_reached.is_set():
-    time.sleep(3)
+    time.sleep(1)
     ser.write(bytes(str(f"delete flash:config.text"), 'ISO-8859-1\r\n'))
     time.sleep(1)
     ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
@@ -59,24 +81,78 @@ if switch_break_reached.is_set():
     time.sleep(1)
     ser.write(bytes(str(f"boot\r"), 'ISO-8859-1'))
 
+elif multilayer_reached.is_set():
+    time.sleep(1)
+    ser.write(bytes(str(f"SWITCH_IGNORE_STARTUP_CFG=1"), 'ISO-8859-1\r'))
+    time.sleep(1)
+    ser.write(bytes(str(f"\r"), 'ISO-8859-1\r'))
+    time.sleep(1)
+    ser.write(bytes(str(f"boot\r"), 'ISO-8859-1'))
+    time.sleep(1)
+    ser.write(bytes(str(f"\r"), 'ISO-8859-1\r'))
+
+elif l2l3_old_reached.is_set():
+    time.sleep(3)
+    ser.write(bytes(str(f"flash_init\r"), 'ISO-8859-1'))
+    while not flash_init_reached.is_set():
+        time.sleep(5)
+        ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
+        if flash_init_reached.is_set(): break
+    time.sleep(3)
+    ser.write(bytes(str(f"delete flash:config.text\r"), 'ISO-8859-1'))
+    time.sleep(2)
+    ser.write(bytes(str(f"y\r"), 'ISO-8859-1'))
+    time.sleep(1)
+    time_now = time.time()
+    while True:
+        if time.time() - time_now > timeout_time:
+            ser.write(bytes(str(f"reset\r"), 'ISO-8859-1'))
+            time.sleep(1)
+            ser.write(bytes(str(f"y\r"), 'ISO-8859-1'))
+            break
+        elif read_only_prompt_reached.is_set():
+            time.sleep(1)
+            ser.write(bytes(str(f"SWITCH_IGNORE_STARTUP_CFG=1"), 'ISO-8859-1\r'))
+            time.sleep(1)
+            ser.write(bytes(str(f"\r"), 'ISO-8859-1\r'))
+            time.sleep(1)
+            ser.write(bytes(str(f"boot\r"), 'ISO-8859-1'))
+            time.sleep(1)
+            ser.write(bytes(str(f"\r"), 'ISO-8859-1\r'))
+            break
+
 else:
     ser.write(bytes(str(f"confreg 0x2142\r"), 'ISO-8859-1'))
     ser.write(bytes(str(f"reset\r"), 'ISO-8859-1'))
 
 while True:
-    if prompt_reached.is_set() and not switch_break_reached.is_set():
+
+    if prompt_reached.is_set() and multilayer_reached.is_set() or prompt_reached.is_set() and l2l3_old_reached.is_set() and read_only_prompt_reached.is_set():
         ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
         ser.write(bytes(str(f"en\r"), 'ISO-8859-1'))
+        time.sleep(1)
         ser.write(bytes(str(f"erase startup-config\r"), 'ISO-8859-1'))
+        time.sleep(1)
         ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
-        ser.write(bytes(str(f"conf t\r"), 'ISO-8859-1'))
-        ser.write(bytes(str(f"config-register 0x2102\r"), 'ISO-8859-1'))
-        ser.write(bytes(str(f"do wr\r"), 'ISO-8859-1'))
+        time.sleep(1)
+        ser.write(bytes(str(f"config t\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"no system ignore startupconfig switch all\r"), 'ISO-8859-1'))
         break
-    elif prompt_reached.is_set() and switch_break_reached.is_set():
+
+    elif prompt_reached.is_set() and switch_break_reached.is_set() or prompt_reached.is_set() and l2l3_old_reached.is_set() and not read_only_prompt_reached.is_set():
         ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
         ser.write(bytes(str(f"no\r"), 'ISO-8859-1'))
         ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
+        break
+
+    elif prompt_reached.is_set() and rommon_reached.is_set():
+        ser.write(bytes(str(f"\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"en\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"erase startup-config\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"yes\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"conf t\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"config-register 0x2102\r"), 'ISO-8859-1'))
+        ser.write(bytes(str(f"do wr\r"), 'ISO-8859-1'))
         break
 
 while True:
